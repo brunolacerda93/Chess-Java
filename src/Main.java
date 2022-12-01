5fd4a0aeffa17e4aea2f5a8016a7b4a18e83811d
Bruno Lacerda <bruno.lacerda9393@gmail.com>
2022-12-01 13:47:24 -0300

    Update: Add classes + Display

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
